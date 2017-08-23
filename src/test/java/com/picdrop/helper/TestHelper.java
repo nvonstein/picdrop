a4e24b2b17cce05639b6ec605bcb39ebbbf0fd0e
Niels von Stein <nvonstein@t-online.de>
2017-08-23 23:22:47 +0200

    Added mock Answers for simple relection of params

--- src/test/java/com/picdrop/helper/TestHelper.java
+++ src/test/java/com/picdrop/helper/TestHelper.java
@@ -5,19 +5,13 @@
  */
 package com.picdrop.helper;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
+import com.picdrop.model.Identifiable;
+import com.picdrop.model.Share;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 import org.bouncycastle.util.Arrays;
-import org.jboss.resteasy.mock.MockHttpRequest;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.mock.web.MockMultipartHttpServletRequest;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,16 +20,16 @@ import org.springframework.web.multipart.MultipartFile;
  * @author nvonstein
  */
 public class TestHelper {
-
+    
     static public byte[] createFileContent(byte[] data, String boundary, String contentType, String fileName) {
         String start = "--" + boundary + "\r\n Content-Disposition: form-data; name=\"file\"; filename=\"" + fileName + "\"\r\n"
                 + "Content-type: " + contentType + "\r\n"
                 + "Content-Transfer-Encoding: binary\r\n\r\n";
-
+        
         String end = "\r\n--" + boundary + "--"; // correction suggested @butfly 
         return Arrays.concatenate(start.getBytes(), data, end.getBytes());
     }
-
+    
     static public HttpServletRequest generateFileRequest(MultipartFile file) throws IOException {
         MockMultipartHttpServletRequest lFileRequest = new MockMultipartHttpServletRequest();
         String boundary = "q1w2e3r4t5y6u7i8o9";
@@ -44,5 +38,25 @@ public class TestHelper {
         lFileRequest.setContent(TestHelper.createFileContent(file.getBytes(), boundary, file.getContentType(), file.getOriginalFilename()));
         lFileRequest.addFile(file);
         return lFileRequest;
+    }
+    
+    public static <T> Answer<T> reflect(int i) {
+        return new Answer<T>() {
+            @Override
+            public T answer(InvocationOnMock arg0) throws Throwable {
+                return arg0.getArgument(i);
+            }
+        };
+    }
+    
+    public static <T extends Identifiable> Answer<T> reflectWithId(int i, String id) {
+        return new Answer<T>() {
+            @Override
+            public T answer(InvocationOnMock arg0) throws Throwable {
+                T obj = arg0.getArgument(i);
+                obj.setId(id);
+                return obj;
+            }
+        };
     }
 }
