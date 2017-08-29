ed77bfe8c30267c4218296f5987969b27dccb0ef
Niels von Stein <nvonstein@t-online.de>
2017-08-29 18:02:29 +0200

    Defined Views and created custom mapper for resteasy

---
+++ src/main/java/com/picdrop/json/Views.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.picdrop.json;
+
+/**
+ *
+ * @author nvonstein
+ */
+public abstract class Views {
+
+    public static abstract class Public {
+    };
+
+    public static abstract class Detailed extends Public {
+    };
+
+    public static abstract class Ignore extends Detailed {
+    };
+
+    public static final class Internal extends Ignore {
+    };
+}
