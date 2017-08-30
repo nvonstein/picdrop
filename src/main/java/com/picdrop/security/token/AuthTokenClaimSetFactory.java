5edf4f0045f94627c5e45ea18210110e436e48b7
Niels von Stein <nvonstein@t-online.de>
2017-08-30 20:12:37 +0200

    implemented verification on AuthTokenClaimsetFactory

--- src/main/java/com/picdrop/security/token/AuthTokenClaimSetFactory.java
+++ src/main/java/com/picdrop/security/token/AuthTokenClaimSetFactory.java
@@ -6,13 +6,22 @@
 package com.picdrop.security.token;
 
 import com.google.common.base.Strings;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import com.nimbusds.jwt.JWTClaimsSet;
+import com.picdrop.model.TokenSet;
+import com.picdrop.model.TokenSetReference;
 import com.picdrop.model.user.RegisteredUser;
 import com.picdrop.model.user.User;
 import com.picdrop.repository.Repository;
+import java.io.IOException;
 import java.text.ParseException;
+import java.util.List;
+import java.util.Random;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 /**
  *
@@ -20,16 +29,18 @@ import java.text.ParseException;
  */
 public class AuthTokenClaimSetFactory extends AbstractClaimSetFactory<User> {
 
+    protected HashFunction hashf;
     protected Repository<String, RegisteredUser> repo;
 
     @Inject
     public AuthTokenClaimSetFactory(Repository<String, RegisteredUser> repo,
-            @Named("service.session.jwt.exp") int jwtExpiry,
+            @Named("service.session.jwt.auth.exp") int jwtExpiry,
             @Named("service.session.jwt.iss") String jwtIssuer,
             @Named("service.session.jwt.aud") String jwtAudience) {
         super(jwtExpiry, jwtIssuer, jwtAudience);
 
         this.repo = repo;
+        this.hashf = Hashing.murmur3_32(new Random().nextInt());
     }
 
     public AuthTokenClaimSetFactory(Repository<String, RegisteredUser> repo) {
@@ -39,24 +50,56 @@ public class AuthTokenClaimSetFactory extends AbstractClaimSetFactory<User> {
     @Override
     public JWTClaimsSet.Builder builder() {
         return super.builder()
+                .jwtID(hashf.hashLong(DateTime.now(DateTimeZone.UTC).getMillis()).toString())
                 .claim("tok", "auth");
     }
-    
+
     @Override
     public User verify(JWTClaimsSet claims) {
+        RegisteredUser user;
         if (verifyGeneralClaims(claims)) {
             return null;
         }
-        
+
         try {
             if (Strings.isNullOrEmpty(claims.getStringClaim("tok"))
                     || !claims.getStringClaim("tok").equals("auth")) {
                 return null;
             }
+
+            user = repo.get(claims.getSubject());
+            if (user == null) {
+                return null;
+            }
+
+            String jti = claims.getJWTID();
+            List<TokenSetReference> tsrefs = user.getTokens();
+            
+            TokenSet ts = null;
+            boolean dirty = false;
+            for (int i = 0; i < tsrefs.size(); i++) {
+                ts = tsrefs.get(i).resolve(true);
+                if (ts == null) {
+                    user = user.removeToken(tsrefs.get(i));
+                    dirty = true;
+                } else if (ts.getAuthJti().equals(jti)) {
+                    break;
+                } else {
+                    ts = null;
+                }
+            }
+
+            if (dirty) {
+                user = repo.update(user.getId(), user); // Update deleted TokenSet refs
+            }
+
+            if (ts == null) {
+                return null;
+            }
         } catch (ParseException ex) {
             return null;
         }
-        
-        return repo.get(claims.getSubject());
+
+        return user;
     }
 }
