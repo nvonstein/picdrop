e46fc603164c72c31bb491b93907d52868c3ddb6
Niels von Stein <nvonstein@t-online.de>
2017-09-05 18:03:15 +0200

    pre work test rework

--- src/main/java/com/picdrop/helper/EnvHelper.java
+++ src/main/java/com/picdrop/helper/EnvHelper.java
@@ -11,11 +11,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -41,6 +37,10 @@ public abstract class EnvHelper {
             reader.close();
         }
         return p;
+    }
+
+    public static void setConfig(Properties config) {
+        EnvHelper.config = config;
     }
 
     protected static Properties getDefaultProperties() {
@@ -92,25 +92,30 @@ public abstract class EnvHelper {
         return config;
     }
 
-    public static Map<String, String> getPropertiesTest() {
-        Map<String, String> p = new HashMap<>();
+    public static Properties getPropertiesTest() {
+        Properties p = new Properties();
 
         p.put("picdrop.validation.email.regex", "^[^@]+[@][^@]+[.][^@]+$");
 
-        p.put("service.session.cookie.enabled", "true");
-        p.put("service.session.cookie.name", "token");
-        p.put("service.session.cookie.domain", "localhost");
-        p.put("service.session.cookie.maxage", "900");
-        p.put("service.session.cookie.http", "true");
-        p.put("service.session.cookie.secure", "false");
-        p.put("service.session.jwt.exp", "900");
-        p.put("service.session.jwt.iss", "picdrop");
+        p.put("service.cookie.enabled", "true");
+        p.put("service.cookie.domain", "localhost");
+        p.put("service.cookie.maxage", "900");
+        p.put("service.cookie.http", "true");
+        p.put("service.cookie.secure", "false");
+
+        p.put("service.cookie.auth.name", "auth");
+        p.put("service.cookie.refresh.name", "refresh");
+
+        p.put("service.jwt.auth.exp", "60"); // 1 Hour
+        p.put("service.jwt.refresh.exp", "43200");    // 30 Days
+        p.put("service.jwt.iss", "picdrop");
+        p.put("service.jwt.aud", "picdrop/app");
 
         p.put("token.signer.alg", "HS256");
         p.put("token.cipher.alg", "dir");
         p.put("token.cipher.meth", "A128CBC-HS256");
 
-        p.put("service.file.store", "");
+        p.put("service.file.store", "/Users/nvonstein/picdrop/store");
 
         p.put("service.upload.store", Files.createTempDir().getAbsolutePath());
         p.put("service.upload.maxmemory", "100000000"); // 100 MB
@@ -118,6 +123,8 @@ public abstract class EnvHelper {
         p.put("service.upload.maxrequestsize", "100000000"); // 100 MB
 
         p.put("service.json.view", "public");
+
+        p.put("service.tika.config", "");
 
         return p;
     }
