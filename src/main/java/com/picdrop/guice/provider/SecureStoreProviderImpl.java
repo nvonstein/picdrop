5f5cddd4b7ff20a3851bd8020abe8c10a25a9814
Niels von Stein <nvonstein@t-online.de>
2017-09-06 17:01:36 +0200

    Impl SecureStore provider

---
+++ src/main/java/com/picdrop/guice/provider/SecureStoreProviderImpl.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.picdrop.guice.provider;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.google.inject.throwingproviders.CheckedProvider;
+import com.picdrop.security.SecureStore;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author nvonstein
+ */
+public class SecureStoreProviderImpl implements SecureStoreProvider {
+
+    Logger log = LogManager.getLogger();
+
+    private final String storePath;
+    private SecureStore ss;
+
+    @Inject
+    public SecureStoreProviderImpl(@Named("service.security.store") String storePath) {
+        this.storePath = storePath;
+    }
+
+    @Override
+    public SecureStore get() throws IOException {
+        if (this.ss == null) {
+            try {
+                ss = new SecureStore(storePath, true);
+            } catch (FileNotFoundException | NoSuchAlgorithmException | CertificateException | KeyStoreException ex) {
+                log.fatal("Unable to load Keystore", ex);
+                throw new IOException(ex.getMessage(), ex);
+            }
+        }
+        return this.ss;
+    }
+}
