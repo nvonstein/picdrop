5948e0cab787e37f4ac0fafa6db24cc12ab0bd28
Niels von Stein <nvonstein@t-online.de>
2017-09-06 17:02:13 +0200

    impl of SymmetricKeyProvider in static version and SecureStore version

---
+++ src/main/java/com/picdrop/guice/provider/SecureStoreSymmetricKeyProvider.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.picdrop.guice.provider;
+
+import com.picdrop.security.SecureStore;
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableEntryException;
+import javax.crypto.SecretKey;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author nvonstein
+ */
+public class SecureStoreSymmetricKeyProvider implements SymmetricKeyProvider {
+
+    Logger log = LogManager.getLogger();
+
+    private SecureStoreProvider ssProv;
+    private final String keyName;
+
+    private SecretKey key;
+
+    public SecureStoreSymmetricKeyProvider(SecureStoreProvider ssProv, String keyName) {
+        this.ssProv = ssProv;
+        this.keyName = keyName;
+    }
+
+    @Override
+    public SecretKey get() throws IOException {
+        if (key == null) {
+            SecureStore ss = this.ssProv.get();
+            try {
+                if (!ss.hasValue(keyName)) {
+                    log.fatal("Keystore does not provide any value for alias '{}'", this.keyName);
+                    throw new IOException(String.format("Keystore does not provide any value for alias '%s'", this.keyName));
+                }
+                key = ss.readValueRaw(keyName);
+            } catch (Exception ex) {
+                log.fatal("Unable to read key from store", ex);
+                throw new IOException(ex.getMessage(), ex);
+            }
+        }
+        return key;
+    }
+
+}
---
+++ src/main/java/com/picdrop/guice/provider/StaticSymmetricKeyProvider.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.picdrop.guice.provider;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
+import java.util.logging.Level;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author nvonstein
+ */
+public class StaticSymmetricKeyProvider implements SymmetricKeyProvider {
+
+    Logger log = LogManager.getLogger();
+
+    private int keysize = 256;
+    private String alg = "AES";
+
+    private SecretKey key;
+
+    public StaticSymmetricKeyProvider(SecretKey key) {
+        this.key = key;
+    }
+
+    public StaticSymmetricKeyProvider(int keysize, String alg) {
+        this.alg = alg;
+        this.keysize = keysize;
+    }
+
+    public StaticSymmetricKeyProvider() {
+    }
+
+    @Override
+    public SecretKey get() throws IOException {
+        if (this.key == null) {
+            try {
+                KeyGenerator kgen = KeyGenerator.getInstance(this.alg);
+                kgen.init(this.keysize);
+                this.key = kgen.generateKey();
+            } catch (Exception ex) {
+                log.fatal(String.format("Unable to generate key with algorithm '%s' and keysize '%d'", this.alg, this.keysize), ex);
+                throw new IOException(ex.getMessage(), ex);
+            }
+        }
+        return key;
+    }
+
+}
