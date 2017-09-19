5881cc0e6e0dcd2098e3804d664ef32f9bc21775
Niels von Stein <nvonstein@t-online.de>
2017-09-19 21:57:14 +0200

    Added unwrap to app exception mapper for better root cause detection

--- src/main/java/com/picdrop/exception/ApplicationExeptionMapper.java
+++ src/main/java/com/picdrop/exception/ApplicationExeptionMapper.java
@@ -5,8 +5,10 @@
  */
 package com.picdrop.exception;
 
+import com.google.common.base.Strings;
 import javax.ws.rs.Produces;
 import javax.ws.rs.ext.Provider;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 
 /**
@@ -21,10 +23,27 @@ public class ApplicationExeptionMapper extends AbstractExceptionMapper<Applicati
         this.log = LogManager.getLogger();
     }
 
-    
+    @Override
+    protected void log(ErrorMessage msg, Throwable e) {
+        Level lvl = msg.lvl;
+        if (lvl == null) {
+            lvl = msg.status >= 500 ? Level.ERROR : Level.DEBUG;
+        }
+        String stringMsg = Strings.isNullOrEmpty(msg.devMessage)
+                ? msg.message
+                : msg.devMessage;
+
+        if (e.getCause() == null) {
+            this.log.log(lvl, stringMsg);
+        } else {
+            this.log.log(lvl, stringMsg, e.getCause());
+        }
+    }
+
     @Override
     protected ErrorMessage processException(ApplicationException ex) {
-        return ex.toErrorMessage(true); // TODO app mode
+
+        return ex.toErrorMessage(true);
     }
 
 }
